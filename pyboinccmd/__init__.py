4ba756a667d0c0d438a85a4bd5104a16c53db119
Scott Phillips <scotty.phillips@hotmail.com>
2020-07-13 08:14:06 +1000

    change from 'run' to 'checkoutput'

--- pyboinccmd/__init__.py
+++ pyboinccmd/__init__.py
@@ -8,6 +8,6 @@ engineer the RPC to the BOINC client
 return: an dict containing the properties of the node.
 """
 def getState(ip_address="127.0.0.1", password=None):
-    raw = subprocess.run(["boinccmd","--host","192.168.1.222","--passwd","password","--get_state"])
+    raw = subprocess.check_output(["boinccmd","--host","192.168.1.222","--passwd","password","--get_state"])
     data = raw.splitlines()
     return data
