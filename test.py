fe749e9b17aee6d9514705ccb9cfdc8eaef8b3f9
Scott Phillips <scotty.phillips@hotmail.com>
2020-07-13 16:23:48 +1000

    test more REGEX

--- test.py
+++ test.py
@@ -3,16 +3,24 @@ import pyboinccmd as pbc
 import re
 
 
-regex = r'========\s\w*\s?\w*?\s========'
-
+regex_title = r'========\s\w*\s?\w*?\s========'
+regex_numeric = r'\d\)\s-----------''
 boinccmd = {}
 category = 'default'
 data = pbc.getState()
 for line in data:
 #    print lines
-    if re.match(regex, line) is not None:
+    if re.match(regex_title , line) is not None:
         category = line.replace('========','').strip().replace(' ','_')
         boinccmd[category] = []
         continue
     boinccmd[category].append(line)
-print(boinccmd)
+if "Projects" in boinccmd:
+    data = boindcmd["projects"]
+    dict = {}
+    dict_id = None
+    for line in data:
+        # if you find a project number create a new dict object
+        if re.match(regex_numeric , line) is not None:
+            dict_id = line.replace(') -----------','')
+        print(dict_id)
